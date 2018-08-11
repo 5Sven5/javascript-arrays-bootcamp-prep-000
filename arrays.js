d2c1da69822fe613e800815699db8fe684084802
Steven C Eaton <steveeaton5@gmail.com>
2018-08-11 23:18:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,4 +4,4 @@ function addElementToBeginningOfArray(array, element){
   [...array, element];
   return array;
 }
-addElementToBeginningOfArray("array", 1)
+addElementToBeginningOfArray("array", 1);
