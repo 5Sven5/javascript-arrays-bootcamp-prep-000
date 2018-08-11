a3a43b2dfc6dead317b32aa1ab19b5db9a232ced
Steven C Eaton <steveeaton5@gmail.com>
2018-08-11 22:13:41 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+const array = [1];
+
+function addElementToBeginningOfArray() {
+array.push('foo');
+}
+
+
+function destructivelyAddElemenToBeginningOfArray() {
+
+}
+function addElementToEndOfArray() {
+
+}
+function destructivelyAddElementToEndOfArray() {
+  
+}
+
+/*Remove functions*/
+function destructivelyRemoveElementFromBeginningOfArray() {
+
+}
+function removeElementFromBeginningOfArray() {
+  
+}
+function destructivelyRemoveElementFromEndOfArray() {
+  
+}
+function removeElementFromEndOfArray() {
+  
+}
